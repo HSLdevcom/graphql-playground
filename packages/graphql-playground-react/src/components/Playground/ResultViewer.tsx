f5f2917072c37bd395b5fb2c68ec50f5a67b9545
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-08-29 16:08:50 +0300

    Fix import

--- packages/graphql-playground-react/src/components/Playground/ResultViewer.tsx
+++ packages/graphql-playground-react/src/components/Playground/ResultViewer.tsx
@@ -7,7 +7,7 @@
  */
 
 import * as React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom'
 import { styled, withProps } from '../../styled'
 
 export interface Props {
