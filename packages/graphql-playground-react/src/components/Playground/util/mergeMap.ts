5141a2b06f604c7e2449774caf068c6bd9f0bbf1
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2018-09-28 13:27:36 +0300

    Add function to merge Map objects

---
+++ packages/graphql-playground-react/src/components/Playground/util/mergeMap.ts
@@ -0,0 +1,29 @@
+export function mergeMap(a: Map<any, any>, b: Map<any, any>) {
+  const output = new Map()
+
+  for (const key of Array.from(a.keys())) {
+    output.set(key, a.get(key))
+  }
+
+  for (const key of Array.from(b.keys())) {
+    const value = b.get(key)
+
+    if (output.has(key)) {
+      if (Array.isArray(output.get(key))) {
+        if (Array.isArray(value)) {
+          output.set(key, value.concat(output.get(key)))
+        } else {
+          const arr = output.get(key)
+          arr.push(value)
+          output.set(key, arr)
+        }
+      } else {
+        output.set(key, Array.of(value, output.get(key)))
+      }
+    } else {
+      output.set(key, value)
+    }
+  }
+
+  return output
+}
